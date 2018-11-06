ab62fc9dc544e557be6925818ca7802bc2c33584
Cesar A. Luis <cesarluis13915@gmail.com>
2018-11-05 18:17:51 -0600

    Enable devtools extension and add logger for dev environment.

--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { applyMiddleware, createStore } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import { promiseMiddleware } from './middlewares';
 import createHistory from 'history/createBrowserHistory';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { createLogger } from 'redux-logger';
 
 import reducer from './reducer';
 
@@ -10,7 +12,16 @@ export const history = createHistory();
 // Build the middleware for intercepting and dispatching navigation actions
 const myRouterMiddleware = routerMiddleware(history);
 
+const getMiddleware = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return applyMiddleware(myRouterMiddleware, promiseMiddleware);
+  } else {
+    // Enable additional logging in non-production environments.
+    return applyMiddleware(myRouterMiddleware, promiseMiddleware, createLogger())
+  }
+};
+
 export const store = createStore(
   reducer,
-  applyMiddleware(myRouterMiddleware, promiseMiddleware)
+  composeWithDevTools(getMiddleware())
 );
